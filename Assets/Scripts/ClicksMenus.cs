27a90c0d780c2cc9178cbe9ae56958bf104ba741
Alberto Beteta Fernández <abf00017@red.ujaen.es>
2020-04-24 18:56:20 +0200

    Base

---
+++ Assets/Scripts/ClicksMenus.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class ClicksMenus : MonoBehaviour, IPointerClickHandler
+{
+	public AudioSource source;
+	public AudioClip audioClick;
+	public void OnPointerClick(PointerEventData eventData)
+	{
+		source.PlayOneShot(audioClick);
+	}
+}
