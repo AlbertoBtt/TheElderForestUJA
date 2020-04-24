cd6948324f8ecf8f6a831e2bc3b9d52af2e37984
Alberto Beteta Fernández <abf00017@red.ujaen.es>
2020-04-24 19:51:52 +0200

    Escena Ajustes

---
+++ Assets/Scripts/CambiarAjustes.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class CambiarAjustes : MonoBehaviour, IPointerClickHandler
+{
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (SceneManager.GetActiveScene().buildIndex == 0)
+        {
+            SceneManager.LoadScene(1);
+        }
+        else
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+}
