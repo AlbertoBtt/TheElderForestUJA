d054b48a17296267e994e7b1fa3a56496f06d449
Alberto Beteta Fernández <abf00017@red.ujaen.es>
2020-04-28 21:17:05 +0200

    Ajustes de Sonido Terminados

---
+++ Assets/Scripts/Sonido/SubirVolumenDoble.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class SubirVolumenDoble : MonoBehaviour, IPointerClickHandler
+{
+	public AudioSource sonido;
+
+	public void OnPointerClick(PointerEventData eventData)
+	{
+		if (sonido.volume < 1)
+		{
+			sonido.volume = sonido.volume + (float)0.1;
+		}
+	}
+}
---
+++ Assets/Scripts/Sonido/actualizarSonido.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class actualizarSonido : MonoBehaviour
+{
+    public AudioSource volumen;
+    public Text texto;
+    void Update()
+    {
+        texto.text = (volumen.volume*100).ToString("000.");
+    }
+}
