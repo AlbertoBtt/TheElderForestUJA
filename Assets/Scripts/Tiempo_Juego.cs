33415e0f1476c6a8f08bcd599a50527e9a90d5de
ferazules1998 <ferazules1998@gmail.com>
2020-04-28 19:27:21 +0200

    Interfaz jugador
    casi acabada

---
+++ Assets/Scripts/Tiempo_Juego.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Tiempo_Juego : MonoBehaviour
+{
+    public Text Tempo;
+    // public float tiempo = 0.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 1;
+        Tempo.text = "" + (int) Time.timeSinceLevelLoad + "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Tempo.text = "" + (int) Time.timeSinceLevelLoad + "";
+    }
+}
