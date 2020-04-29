ddbba2ae2afff407cee2c787a0d7b0af0c680ccd
ferazules1998 <ferazules1998@gmail.com>
2020-04-29 16:53:16 +0200

    Tiempo camara

---
+++ Assets/Scripts/Tiempo_Camara.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Tiempo_Camara : MonoBehaviour
+{
+    public Text Tiempo;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Tiempo.text = "" + (int) Time.timeSinceLevelLoad;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Tiempo.text = "" + (int)Time.timeSinceLevelLoad;
+    }
+}
