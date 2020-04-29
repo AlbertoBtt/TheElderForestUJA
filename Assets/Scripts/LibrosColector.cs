abb741e7d292bd5540922f85862d47afb81d07be
ferazules1998 <ferazules1998@gmail.com>
2020-04-29 19:23:12 +0200

    version alpha

--- Assets/Scripts/LibrosColector.cs
+++ Assets/Scripts/LibrosColector.cs
@@ -8,6 +8,12 @@ public class LibrosColector : MonoBehaviour
     public static int puntos = 0;
     public Text score;
     public AudioClip sonidopagina = null;
+    public Text notificacion;
+
+    private void Start()
+    {
+        notificacion.gameObject.SetActive(false);
+    }
 
     void OnTriggerEnter(Collider collision) 
     {
@@ -15,9 +21,19 @@ public class LibrosColector : MonoBehaviour
         {
             puntos += 1;
             AudioSource.PlayClipAtPoint(sonidopagina, transform.position, 1.0f);
+            StartCoroutine(mostrar_texto());
             Destroy(collision.gameObject);
+            
         }
     }
+
+    IEnumerator mostrar_texto()
+    {
+        notificacion.gameObject.SetActive(true);
+        yield return new WaitForSeconds(3);
+        notificacion.gameObject.SetActive(false);
+    }
+
 
     // Update is called once per frame
     void Update()
--- Assets/Scripts/Tiempo_Camara.cs
+++ Assets/Scripts/Tiempo_Camara.cs
@@ -6,15 +6,25 @@ using UnityEngine.UI;
 public class Tiempo_Camara : MonoBehaviour
 {
     public Text Tiempo;
+    float gametimer = 0f;
     // Start is called before the first frame update
     void Start()
     {
-        Tiempo.text = "" + (int) Time.timeSinceLevelLoad;
+        //Tiempo.text = "" + (int) Time.timeSinceLevelLoad;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Tiempo.text = "" + (int)Time.timeSinceLevelLoad;
+        //Tiempo.text = "" + (int)Time.timeSinceLevelLoad;
+        gametimer += Time.deltaTime;
+
+        int segundo = (int)(gametimer % 60);
+        int minuto = (int)(gametimer / 60) & 60;
+        int hora = (int)(gametimer / 3600) % 24;
+
+        string timerstring = string.Format("{0:0}:{1:00}:{2:00}", hora, minuto, segundo);
+
+        Tiempo.text = timerstring;
     }
 }
