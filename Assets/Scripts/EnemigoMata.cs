f0090d7566b062b8e41a590351087ed98dd70108
Dimas1998 <dimaquono@gmail.com>
2020-04-30 19:05:53 +0200

    "Slender va despacio"

---
+++ Assets/Scripts/EnemigoMata.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemigoMata : MonoBehaviour
+{
+    public AudioClip muerte = null;
+
+
+    void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.tag == "Enemigo")
+        {
+            AudioSource.PlayClipAtPoint(muerte, transform.position, 1.0f);
+            Time.timeScale = 0; 
+        }
+    }
+
+}
---
+++ Assets/Scripts/LinternaEnemigo.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinternaEnemigo : MonoBehaviour
+{
+    public Camera cam;
+    public GameObject luz;
+    public GameObject Enemigos;
+    private float velocidad_antigua;
+
+
+    // Update is called once per frame
+    void Start() 
+    {
+        velocidad_antigua = Enemigos.GetComponent<Enemigo>().velocidad;
+    }
+
+    void Update()
+    {
+        if (luz.activeSelf) 
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(cam.transform.position, cam.transform.forward, out hit, 10))
+            {
+                if (hit.collider.gameObject.tag == "Enemigo")
+                {
+                    Enemigos.GetComponent<Enemigo>().velocidad = 0.2f;
+                }
+                
+            }
+            else
+            {
+                Enemigos.GetComponent<Enemigo>().velocidad = velocidad_antigua;
+            }
+        }else
+            {
+                Enemigos.GetComponent<Enemigo>().velocidad = velocidad_antigua;
+            }
+    }
+
+}
